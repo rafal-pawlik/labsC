2393892db7249cb4cf27cb8b26ed1561caa2fe5b
Valaraucoo <jestem.kamil.wozniak@gmail.com>
2019-03-18 00:42:06 +0100

    xd

--- lab3/zad2.cpp
+++ lab3/zad2.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <ostream>
 #include <cmath>
+#include <utility>
 #include <iomanip>
 
 
@@ -18,11 +19,23 @@ struct SmartTree {
 
 
 std::unique_ptr <SmartTree> CreateLeaf(int value);
-std::unique_ptr <SmartTree> InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree);
-std::unique_ptr <SmartTree> InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree);
+
+std::unique_ptr <SmartTree>& InsertLeftChild(std::unique_ptr<SmartTree> &tree, std::unique_ptr<SmartTree> left_subtree);
+std::unique_ptr <SmartTree>& InsertRightChild(std::unique_ptr<SmartTree> &tree, std::unique_ptr<SmartTree> right_subtree);
+
 void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out);
-std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
-// std::unique_ptr <SmartTree> RestoreTree(const std::string &tree);
+void PrintTreePreOrder(const std::unique_ptr<SmartTree> &unique_ptr,std::ostream *out);
+
+std::string DumpTreeInOrder(const std::unique_ptr<SmartTree> &tree);
+std::string DumpTreePreOrder(const std::unique_ptr<SmartTree> &tree);
+
+
+std::unique_ptr<SmartTree> BuildTree(const std::vector<int>& vIn ,const std::vector<int>& vPre, int inS , int inE );
+std::unique_ptr<SmartTree> RestoreTree(const std::string& sIn ,const std::string& sPre);
+
+
+std::vector<int> SplitStringToVector(std::string str , char sep);
+
 
 /*
     ||\\   //||     //\\    ||   ||\\   ||
@@ -30,23 +43,37 @@ std::string DumpTree(const std::unique_ptr<SmartTree> &tree);
     ||       ||   ||    ||  ||   ||  \\ ||
     ||       ||   ||    ||  ||   ||   \\||
 */
+
+
+
 int main(int argc , char** argv ){
 
     std::unique_ptr<SmartTree> tree = CreateLeaf(1);
-    std::unique_ptr<SmartTree> left = CreateLeaf(2);
-    std::unique_ptr<SmartTree> right = CreateLeaf(3);
-    std::unique_ptr<SmartTree> leftleft = CreateLeaf(4);
 
-    left = InsertLeftChild(std::move(left), std::move(leftleft));
+    std::unique_ptr<SmartTree> &left = InsertLeftChild(tree, CreateLeaf(2));
+    std::unique_ptr<SmartTree> &right = InsertRightChild(tree, CreateLeaf(3));
 
-    tree = InsertLeftChild(std::move(tree) , std::move(left));
-    tree = InsertRightChild(std::move(tree) , std::move(right));
+    InsertLeftChild(left, CreateLeaf(4));
+    InsertRightChild(left, CreateLeaf(5));
+    InsertLeftChild(right, CreateLeaf(6));
+    InsertRightChild(right, CreateLeaf(7));
+/*
+            1
+         /    \
+        2      3
+      /  \    /  \
+     4   5   6    7
+*/
 
-    std::cout << "Tree in order: " ;
 
-    PrintTreeInOrder(tree , &std::cout);
 
-    std::cout<< "\n" << DumpTree(tree) << "\n";
+
+    auto new_tree = RestoreTree(DumpTreeInOrder(tree) , DumpTreePreOrder(tree));
+
+
+    auto inorder = DumpTreeInOrder(new_tree);
+    std::cout << inorder << '\n';
+
 
 
 
@@ -68,38 +95,99 @@ std::unique_ptr <SmartTree> CreateLeaf(int value){
     tree-> left = nullptr;
     tree-> right = nullptr;
 
-    return tree;
+    return std::move(tree);
 }
 
-std::unique_ptr <SmartTree> InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree){
-    std::unique_ptr<SmartTree> new_tree = std::move(tree);
-    new_tree -> left = std::move(left_subtree);
+std::unique_ptr <SmartTree>& InsertLeftChild(std::unique_ptr<SmartTree> &tree, std::unique_ptr<SmartTree> left_subtree){
 
-    return new_tree;
+    tree->left = std::move(left_subtree);
+    return tree->left;
 }
 
-std::unique_ptr <SmartTree> InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree){
-    std::unique_ptr<SmartTree> new_tree = std::move(tree);
-    new_tree -> right = std::move(right_subtree);
+std::unique_ptr <SmartTree>& InsertRightChild(std::unique_ptr<SmartTree> &tree, std::unique_ptr<SmartTree> right_subtree){
 
-    return new_tree;
+    tree->right = std::move(right_subtree);
+    return tree->right;
 }
 
-void PrintTreeInOrder(const std::unique_ptr<SmartTree>& unique_ptr,std::ostream *out){
+void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr,std::ostream *out){
 
-    if(unique_ptr == nullptr) return ;
+    if(unique_ptr == nullptr) return ; // if ptr is null - break
 
     PrintTreeInOrder(std::move(unique_ptr -> left), out);
-    *out << unique_ptr -> data << ",";
+    *out << unique_ptr -> data << " ";
     PrintTreeInOrder(std::move(unique_ptr -> right), out);
 }
 
-std::string DumpTree(const std::unique_ptr<SmartTree> &tree){
+void PrintTreePreOrder(const std::unique_ptr<SmartTree> &unique_ptr,std::ostream *out){
+    if(unique_ptr == nullptr) return ;
 
+    *out << unique_ptr -> data << " ";
+    PrintTreeInOrder(std::move(unique_ptr -> left), out);
+    PrintTreeInOrder(std::move(unique_ptr -> right), out);
+}
+
+std::string DumpTreeInOrder(const std::unique_ptr<SmartTree> &tree){
+    // convert tree -> string  in order tree
     std::ostringstream oss;
-
-    PrintTreeInOrder(std::move(tree -> left), &oss);
-    PrintTreeInOrder(std::move(tree -> right), &oss);
+    PrintTreeInOrder(std::move(tree), &oss);
 
     return oss.str();
+}
+
+std::string DumpTreePreOrder(const std::unique_ptr<SmartTree> &tree){
+    // convert tree -> string  pre order tree
+    std::ostringstream oss;
+    PrintTreePreOrder(std::move(tree), &oss);
+
+    return oss.str();
+}
+
+
+
+std::unique_ptr<SmartTree> BuildTree(const std::vector<int>& vIn ,const std::vector<int>& vPre, int inS , int inE ){
+
+    static int preIndex = 0;
+
+    if(inS > inE) return nullptr;
+
+    auto tree = CreateLeaf(vPre[preIndex++]);
+
+    if(inS == inE ) return tree;
+
+    int target = tree -> data;
+    int inIndex = 0;
+    for(auto& x: vIn){
+        if(x == target){
+            break;
+        }
+        inIndex++;
+    }
+
+    InsertLeftChild(tree,BuildTree(vIn , vPre , inS , inIndex - 1));
+    InsertRightChild(tree,BuildTree(vIn , vPre , inIndex + 1 , inE));
+
+    return std::move(tree);
+}
+
+std::unique_ptr<SmartTree> RestoreTree(const std::string& sIn ,const std::string& sPre){
+    std::vector<int> vIn = SplitStringToVector(sIn , ' ');
+    std::vector<int> vPre = SplitStringToVector(sPre , ' ');
+    std::unique_ptr<SmartTree> tree = BuildTree(vIn,vPre,0 , vIn.size() -1);
+    return std::move(tree);
+}
+
+std::vector<int> SplitStringToVector(std::string str , char sep){
+    std::vector<int> result;
+    std::string buff ="";
+
+    for(auto c: str){
+        if(c != sep){
+            buff += c;
+        } else {
+            result.push_back(std::stoi(buff));
+            buff = "";
+        }
+    }
+    return result;
 }
